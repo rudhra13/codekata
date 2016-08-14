71ba0ea6fc975534baf548861f5aab92c2d5149d
rudhra rai <rudhra rai>
2016-08-15 00:45:21 +0530

    check vowel

---
+++ chkvowel.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char data;
+	cin>>data;
+	
+	switch(data)
+	{
+	case 'a':
+		cout<<"vowel";
+		break;
+	case 'e':
+		cout<<"vowel";
+		break;
+	case 'i':
+		cout<<"vowel";
+		break;
+	case 'o':
+		cout<<"vowel";
+		break;
+	case 'u':
+		cout<<"vowel";
+		break;
+	default:
+		cout<<"consonent";	
+		break;
+	}
+}
