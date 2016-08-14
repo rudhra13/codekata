106d00da26445d14aba25b68b967935f648fd42c
rudhra rai <rudhra rai>
2016-08-15 00:50:27 +0530

    check character

---
+++ chkchar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char data;
+	cin>>data;
+	if((data>='a' && data<='z')|| (data>='A' && data<='Z') )
+	cout<<"character";
+	else
+	cout<<"not character";
+return 0;
+}
